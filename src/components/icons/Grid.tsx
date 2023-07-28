9c34b66d2ef29e83052f5ae5f41e2c89e76ddb43
TheKelvinT <tnchnkit@hotmail.com>
2023-07-28 15:41:33 +0800

    deployment error fix

--- src/components/icons/Grid.tsx
+++ src/components/icons/Grid.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
-
-
 const Grid = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
