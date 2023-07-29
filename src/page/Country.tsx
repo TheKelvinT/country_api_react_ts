d28779e81a585b6511932d40efea9f6df98a3ae2
TheKelvinT <tnchnkit@hotmail.com>
2023-07-29 15:14:24 +0800

    loading enhacement

--- src/page/Country.tsx
+++ src/page/Country.tsx
@@ -5,7 +5,7 @@ import SingleCard from '../components/SingleCard';
 import {Link} from 'react-router-dom'
 import Emblem from '../assets/un.svg'
 import { shuffleArray } from '../utils/utils';
-import {Tooltip, Skeleton} from 'antd'
+import {Tooltip} from 'antd'
 import Loading from '../components/Loading';
 
 const Country = () => {
@@ -35,6 +35,9 @@ const Country = () => {
     fetchData();
   }, [id]);
 
+  if (!country || loading){
+    return <Loading type='large'/>
+  }
   return (
     <div className="flex flex-col justify-center mt-36  ">
      {country && <div className='text-sm mb-12 md:mx-0 text-slate-500 dark:text-slate-300'>
@@ -43,11 +46,10 @@ const Country = () => {
       <div className=" xl:max-w-[1120px] flex md:flex-row flex-col items-center  gap-16  shadow-xl dark:bg-slate-700 h-10/12 p-8 md:p-12">
         {/* Country Flag */}
         <div className="basis-1/2 w-full h-[256px] shadow-md">
-          {!country || loading ?   <Skeleton.Image active style={{height:"256px", width:"320px"}}/> :  <img src={country?.flags?.png} alt={country?.name.common} className="w-full h-full object-fit" />  }
+        <img src={country?.flags?.png} alt={country?.name.common} className="w-full h-full object-fit" /> 
         </div>
 
         {/* Country Information */}
-        {!country || loading ? <Skeleton active/> : 
         <div className="col-span-1 lg:col-span-3 w-full basis-1/2">
         <div className='flex justify-between items-center mb-4 gap-6'>
       <h1 className="text-2xl font-bold">{country?.name.common}</h1>
@@ -108,14 +110,14 @@ const Country = () => {
           )}
           </div>
         </div>
-        }
+       
       </div>
       
       <div className='text-center my-20'>
           <h1 className='text-2xl font-bold px-4'>
             Other Countries You May Like
           </h1>
-        {!country || loading ? <Loading type="small"/> :
+       
         <div className='mt-12 flex justify-center gap-8'>  
         {shuffledCountry && (
           <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8 justify-center'>
@@ -125,7 +127,7 @@ const Country = () => {
           </div>
         )}
         </div>
-        }
+        
       </div>
     </div>
   );
